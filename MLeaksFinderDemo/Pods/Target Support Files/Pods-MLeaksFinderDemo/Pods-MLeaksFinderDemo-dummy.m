5a7e801e810487c090ae78d3e77b001b7dcd192f
LeeWongSnail <wangli_0632@163.com>
2018-04-15 18:23:39 +0800

    add memoryleak

--- MLeaksFinderDemo/Pods/Target Support Files/Pods-MLeaksFinderDemo/Pods-MLeaksFinderDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MLeaksFinderDemo : NSObject
-@end
-@implementation PodsDummy_Pods_MLeaksFinderDemo
-@end
--- MLeaksFinderDemo/Pods/Target Support Files/Pods-MLeaksFinderDemoTests/Pods-MLeaksFinderDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MLeaksFinderDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_MLeaksFinderDemoTests
-@end
