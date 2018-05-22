742026affa111291ff9f04e2ba3d846daf4df00b
LeeWongSnail <wangli_0632@163.com>
2018-05-22 11:22:12 +0800

    add ui

---
+++ OOCalculator/OOCalculator/Operator.h
@@ -0,0 +1,16 @@
+//
+//  Operator.h
+//  OOCalculator
+//
+//  Created by LeeWong on 2018/5/22.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Operator : NSObject
+
+
+
+@end
---
+++ OOCalculator/OOCalculator/Operator.m
@@ -0,0 +1,13 @@
+//
+//  Operator.m
+//  OOCalculator
+//
+//  Created by LeeWong on 2018/5/22.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import "Operator.h"
+
+@implementation Operator
+
+@end
