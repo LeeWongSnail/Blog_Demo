bf05c6a65cce5bebea7adad5b6e4a006597675f2
LeeWongSnail <wangli_0632@163.com>
2021-01-02 11:08:30 +0800

    [comments]：新增类结构demo [jira_id]：none [version]：

---
+++ ObjectStructure/ObjectStructure/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjectStructure
+//
+//  Created by LeeWong on 2021/1/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjectStructure/ObjectStructure/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ObjectStructure
+//
+//  Created by LeeWong on 2021/1/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
